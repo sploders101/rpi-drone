bdf62a8247fd28fb00af9f0e1b60f527b1c84d4c
Shaun Keys <mariobuddy@gmail.com>
2018-07-28 15:44:33 -0600

    Declare projectDir before spawning subsystem

--- node/index.js
+++ node/index.js
@@ -5,9 +5,6 @@ let spawn = require("child_process").spawn;
 let path = require("path");
 let SteamController = require("node-steam-controller");
 
-// SPAWN PYTHON SUBSYSTEM ON CORE 3
-let fc = spawn("taskset",["-c","3","python3","python/main.py"],{cwd: projectDir});
-
 // INITIALIZE VARS
 let projectDir = path.join(__dirname,"..");
 let sc = new SteamController();
@@ -18,6 +15,9 @@ let control = {
 	r: 0,
 	throttle: 0
 };
+
+// SPAWN PYTHON SUBSYSTEM ON CORE 3
+let fc = spawn("taskset",["-c","3","python3","python/main.py"],{cwd: projectDir});
 
 // SETUP PYTHON IPC
 fc.stdout.setEncoding("ascii");
