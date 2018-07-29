5b772da8ca8be26595b2a379afa23ff259fd5ffe
Shaun Keys <mariobuddy@gmail.com>
2018-07-28 20:25:45 -0600

    flip x and y

--- python/quad_stablizer.py
+++ python/quad_stablizer.py
@@ -3,8 +3,8 @@ sensorMultiplier = 0.125;
 def stablizer(sensors,control):
 	motors = [0,0,0,0];
 	if control["throttle"] > 0:
-		motors[0] = ( sensors["x"]/2)*sensorMultiplier + ( sensors["y"]/2)*sensorMultiplier + ( control["x"]/2) + (-control["y"]/2) + (-control["rotate"]/2) + (control["throttle"]);
-		motors[1] = (-sensors["x"]/2)*sensorMultiplier + ( sensors["y"]/2)*sensorMultiplier + (-control["x"]/2) + (-control["y"]/2) + ( control["rotate"]/2) + (control["throttle"]);
-		motors[2] = ( sensors["x"]/2)*sensorMultiplier + (-sensors["y"]/2)*sensorMultiplier + ( control["x"]/2) + ( control["y"]/2) + ( control["rotate"]/2) + (control["throttle"]);
-		motors[3] = (-sensors["x"]/2)*sensorMultiplier + (-sensors["y"]/2)*sensorMultiplier + (-control["x"]/2) + ( control["y"]/2) + (-control["rotate"]/2) + (control["throttle"]);
+		motors[0] = ( sensors["y"]/2)*sensorMultiplier + ( sensors["x"]/2)*sensorMultiplier + ( control["x"]/2) + (-control["y"]/2) + (-control["rotate"]/2) + (control["throttle"]);
+		motors[1] = (-sensors["y"]/2)*sensorMultiplier + ( sensors["x"]/2)*sensorMultiplier + (-control["x"]/2) + (-control["y"]/2) + ( control["rotate"]/2) + (control["throttle"]);
+		motors[2] = ( sensors["y"]/2)*sensorMultiplier + (-sensors["x"]/2)*sensorMultiplier + ( control["x"]/2) + ( control["y"]/2) + ( control["rotate"]/2) + (control["throttle"]);
+		motors[3] = (-sensors["y"]/2)*sensorMultiplier + (-sensors["x"]/2)*sensorMultiplier + (-control["x"]/2) + ( control["y"]/2) + (-control["rotate"]/2) + (control["throttle"]);
 	return motors;
