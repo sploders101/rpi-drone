a100f3dc477836ce401adfea207281a63c950faf
Shaun Keys <mariobuddy@gmail.com>
2018-07-28 20:40:48 -0600

    Don't let values go negative

--- python/quad_stablizer.py
+++ python/quad_stablizer.py
@@ -7,4 +7,9 @@ def stablizer(sensors,control):
 		motors[1] = (-sensors["y"]/2)*sensorMultiplier + ( sensors["x"]/2)*sensorMultiplier + (-control["x"]/2) + (-control["y"]/2) + ( control["rotate"]/2) + (control["throttle"]);
 		motors[2] = ( sensors["y"]/2)*sensorMultiplier + (-sensors["x"]/2)*sensorMultiplier + ( control["x"]/2) + ( control["y"]/2) + ( control["rotate"]/2) + (control["throttle"]);
 		motors[3] = (-sensors["y"]/2)*sensorMultiplier + (-sensors["x"]/2)*sensorMultiplier + (-control["x"]/2) + ( control["y"]/2) + (-control["rotate"]/2) + (control["throttle"]);
+
+		for value in motors:
+			if value < 0:
+				return [control["throttle"],control["throttle"],control["throttle"],control["throttle"]];
+
 	return motors;
