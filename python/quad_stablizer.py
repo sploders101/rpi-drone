8bfb1210f0326df9406df074483a30a6884280a2
Shaun Keys <mariobuddy@gmail.com>
2018-08-04 15:20:38 -0600

    removed print statement

--- python/quad_stablizer.py
+++ python/quad_stablizer.py
@@ -5,8 +5,6 @@ sensorMultiplier = 0.125;
 def stablizer(sensors,control):
 	motors = [0,0,0,0];
 	if control["throttle"] > 0:
-		print(control["throttle"]);
-		sys.stdout.flush();
 		motors[0] = ( sensors["y"]/2)*sensorMultiplier + ( sensors["x"]/2)*sensorMultiplier + ( control["x"]/2) + (-control["y"]/2) + (-control["rotate"]/2) + (control["throttle"]);
 		motors[1] = (-sensors["y"]/2)*sensorMultiplier + ( sensors["x"]/2)*sensorMultiplier + (-control["x"]/2) + (-control["y"]/2) + ( control["rotate"]/2) + (control["throttle"]);
 		motors[2] = ( sensors["y"]/2)*sensorMultiplier + (-sensors["x"]/2)*sensorMultiplier + ( control["x"]/2) + ( control["y"]/2) + ( control["rotate"]/2) + (control["throttle"]);
