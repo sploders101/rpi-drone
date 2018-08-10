480490fe7aca83ec3f7d3e4730f90bc8ea97ddd9
Shaun Keys <mariobuddy@gmail.com>
2018-08-10 17:58:41 -0600

    Bugfixes for transferring to pi

--- node/mmapTest.js
+++ node/mmapTest.js
@@ -1,5 +1,5 @@
 let fs = require("fs");
-let mmap = require("/home/shaun/.nvm/versions/node/v8.9.4/lib/node_modules/mmap.js");
+let mmap = require("mmap.js");
 
 const fd = fs.openSync("/run/user/1000/mmapTest","r+");
 let fileBuf = mmap.alloc(
