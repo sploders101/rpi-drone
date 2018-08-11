fa2cf176bd6a447939216afce4e1b5a92e658b9e
Shaun Keys <mariobuddy@gmail.com>
2018-08-10 19:23:55 -0600

    Add untested I2C support via wiringpi

--- cpp/flightController.c
+++ cpp/flightController.c
@@ -1,16 +1,29 @@
-#include <iostream>
+#include <iostream> // Print statements (cout << ...)
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/mman.h>
+#include <sys/mman.h> // mmap
+#include <wiringPiI2C.h> // i2c libraries
 
 #define GYROADDR 0x1c
-#define MMAPSIZE 16
+#define GYROX 0x28
+#define GYROY 0x2a
+#define MMAPSIZE 22
 #define MMAPLOCATION "/run/user/1000/mmapTest"
 
 // Declare memory mapped variables
 int sharedMem;
 void *mappedBuffer;
 short *throttle;
+short *moveX;
+short *moveY;
+short *moveRot;
+short *gX;
+short *gY;
+short *gZ;
+short *bar;
+short *cX;
+short *cY;
+short *sensorMult;
 
 int main() {
 	// Setup mmap for sharing variables with node
@@ -23,12 +36,28 @@ int main() {
 
 	// Setup memory locations
 	throttle = (short *) mappedBuffer;
+	moveX = (short *) mappedBuffer + ( sizeof(short) * 1 );
+	moveY = (short *) mappedBuffer + ( sizeof(short) * 2 );
+	moveRot = (short *) mappedBuffer + ( sizeof(short) * 3);
+	gX = (short *) mappedBuffer + ( sizeof(short) * 4);
+	gY = (short *) mappedBuffer + ( sizeof(short) * 5);
+	gZ = (short *) mappedBuffer + ( sizeof(short) * 6);
+	bar = (short *) mappedBuffer + ( sizeof(short) * 7);
+	cX = (short *) mappedBuffer + ( sizeof(short) * 8);
+	cY = (short *) mappedBuffer + ( sizeof(short) * 9);
+	sensorMult = (short *) mappedBuffer + ( sizeof(short) * 10);
 
-	while(*throttle == 0) {
+	// Setup I2C devices
+	int gyro = wiringPiI2CSetup(GYROADDR);
+
+	while(1) {
+		*gX = wiringPiI2CReadReg16(gyro,GYROX);
+		*gY = wiringPiI2CReadReg16(gyro,GYROY);
+		std::cout << *gX << "\n";
 		sleep(0);
 	}
 
-	std::cout << *throttle << "\n";
+
 
 	close(sharedMem);
 
